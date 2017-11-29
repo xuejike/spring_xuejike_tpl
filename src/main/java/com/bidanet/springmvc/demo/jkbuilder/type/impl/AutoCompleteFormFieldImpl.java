b308c5b29f11cf9094f0dbd980cddf4025b9585d
xuejike <xuejike2004@126.com>
2017-11-29 14:02:12 +0800

    自动完成

--- src/main/java/com/bidanet/springmvc/demo/jkbuilder/type/impl/AutoCompleteFormFieldImpl.java
+++ src/main/java/com/bidanet/springmvc/demo/jkbuilder/type/impl/AutoCompleteFormFieldImpl.java
@@ -1,11 +1,13 @@
 package com.bidanet.springmvc.demo.jkbuilder.type.impl;
 
+import com.bidanet.bdcms.core.common.SpringWebTool;
 import com.bidanet.springmvc.demo.jkbuilder.annotation.ext.JkAutoCompleteFormField;
 import com.bidanet.springmvc.demo.jkbuilder.annotation.type.AutoCompleteType;
 import com.bidanet.springmvc.demo.jkbuilder.data.JkNameValueData;
 import com.bidanet.springmvc.demo.jkbuilder.type.AbsBaseFormField;
 import com.bidanet.springmvc.demo.jkbuilder.type.AbsDataSourceFormField;
 import com.bidanet.springmvc.demo.jkbuilder.type.FormFieldInfo;
+import com.bidanet.springmvc.demo.jkbuilder.type.JkTypeDataSource;
 import lombok.Data;
 
 import java.util.List;
@@ -24,14 +26,29 @@ public class AutoCompleteFormFieldImpl extends AbsDataSourceFormField {
         super.addExpansionData(info, map);
 
         map.put("auto",jkAutoCompleteFormField);
-
-        if (jkAutoCompleteFormField.type()== AutoCompleteType.remote){
-            for (JkNameValueData data : dataSource) {
-                if (data.getValue().equals(info.getVal())){
-                    return;
+        if (info.getVal()!=null){
+            if (jkAutoCompleteFormField.type()== AutoCompleteType.remote){
+                for (JkNameValueData data : dataSource) {
+                    if (data.getValue().equals(info.getVal())){
+                        return;
+                    }
                 }
-            }
+                //加载当前选中值
+                Class<? extends JkTypeDataSource> beanCls = jkDataSource.beanCls();
+                String text = SpringWebTool.getBean(beanCls).getText(info.getVal().toString());
+                dataSource.add(0, new JkNameValueData() {
+                    @Override
+                    public String getName() {
+                        return text;
+                    }
 
+                    @Override
+                    public String getValue() {
+                        return info.getVal().toString();
+                    }
+                });
+            }
         }
+
     }
 }

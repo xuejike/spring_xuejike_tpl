8878df3e28a1f90a048d5df3b0cddcfda1541fde
xuejike <xuejike2004@126.com>
2017-12-09 16:20:44 +0800

    新增支持表单jkDisable 注解

--- src/main/java/com/bidanet/springmvc/demo/jkbuilder/annotation/JkDisable.java
+++ src/main/java/com/bidanet/springmvc/demo/jkbuilder/annotation/JkDisable.java
@@ -2,7 +2,7 @@ package com.bidanet.springmvc.demo.jkbuilder.annotation;
 
 import java.lang.annotation.*;
 
-@Target({ElementType.FIELD,ElementType.ANNOTATION_TYPE})
+@Target({ElementType.FIELD,ElementType.ANNOTATION_TYPE,ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface JkDisable {
